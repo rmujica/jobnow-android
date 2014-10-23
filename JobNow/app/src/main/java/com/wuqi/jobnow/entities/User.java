409b10b7548558f46ab9e4bb5886947e8775cd08
René Mujica Moreau <rene.mujica.m@gmail.com>
2014-10-23 13:48:29 -0300

    checking if user is business or not, new api call

--- JobNow/app/src/main/java/com/wuqi/jobnow/entities/User.java
+++ JobNow/app/src/main/java/com/wuqi/jobnow/entities/User.java
@@ -2,9 +2,18 @@ package com.wuqi.jobnow.entities;
 
 import com.google.gson.annotations.SerializedName;
 
+import java.util.List;
+
 public class User {
     @SerializedName("_id") public String id;
     public String type;
     public String email;
     public String password;
+    public String first_name;
+    public String last_name;
+    public String birth_date;
+    public String location;
+    public String occupation;
+    public String facebook_user;
+    public List<String> applications;
 }
